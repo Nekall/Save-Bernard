2456c02d7141541fc89ecd45b4c033886ff08cef
Neka <nekoanchor@yahoo.fr>
2022-05-14 12:22:03 +0200

    style: add seaweed

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,8 +4,6 @@ import { Link } from "react-router-dom";
 const Navbar = () => (
   <nav className="navbar">
     <Link to="/">Home</Link>
-    <Link to="/page-1">Page 1</Link>
-    <Link to="/page-2">Page 2</Link>
   </nav>
 );
 
---
+++ src/components/Seaweed/index.jsx
@@ -0,0 +1,5 @@
+import "./style.scss";
+
+const Seaweed = () => <div className="seaweed"></div>;
+
+export default Seaweed;
